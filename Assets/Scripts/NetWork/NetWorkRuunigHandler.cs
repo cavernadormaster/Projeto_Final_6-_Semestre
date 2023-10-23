cc1427d0a1ea1ce71b15bf29b1d6102a20ba53e9
Felipe dos Santos Moraes <felipemoraes2002@gmail.com>
2023-10-23 17:02:59 -0300

    Photon Fusion test Complete and still working

--- Assets/Scripts/NetWork/NetWorkRuunigHandler.cs
+++ Assets/Scripts/NetWork/NetWorkRuunigHandler.cs
@@ -1,14 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using Fusion;
+using Fusion.Sockets;
+using System.Threading.Tasks;
+using System;
+using System.Linq;
 
 public class NetWorkRuunigHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public NetworkRunner networkRunnerPrefab;
+    NetworkRunner networkRunner;
+
     void Start()
     {
-        
+        networkRunner = Instantiate(networkRunnerPrefab);
+        networkRunner.name = "Net Test";
+
+        var clientTask = IniTializeNetworkRunner(networkRunner, GameMode.AutoHostOrClient, 
+                                                 NetAddress.Any(), SceneManager.GetActiveScene().buildIndex, null);
+
+        Debug.Log("Server NetWork Started");
     }
 
-   
+   protected virtual Task IniTializeNetworkRunner(NetworkRunner runner, GameMode gameMode, NetAddress address, SceneRef scene, Action<NetworkRunner> initialized)
+    {
+        var scenemanager = runner.GetComponents(typeof(MonoBehaviour)).OfType<INetworkSceneManager>().FirstOrDefault();
+
+        if(scenemanager == null)
+        {
+            scenemanager = runner.gameObject.AddComponent<NetworkSceneManagerDefault>(); 
+        }
+
+        runner.ProvideInput = true;
+
+        return runner.StartGame(new StartGameArgs
+        {
+            GameMode = gameMode,
+            Address = address,
+            Scene = scene,
+            SessionName = "SampleScene",
+            Initialized = initialized,
+            SceneManager = scenemanager
+        });
+    }
+
 }
