cc80f0c5e87663b811cd7b125b853e7194e260d0
Felipe Moraes <89394365+cavernadormaster@users.noreply.github.com>
2023-10-31 08:47:47 -0300

    Nome do Jogador Adicionado

--- Assets/Scripts/NetWork/NetWorkPlayer.cs
+++ Assets/Scripts/NetWork/NetWorkPlayer.cs
@@ -2,10 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Fusion;
+using TMPro;
 
 public class NetWorkPlayer : NetworkBehaviour, IPlayerLeft
 {
+    public TextMeshProUGUI playerNickNameTM;
     public static NetWorkPlayer Local { get; set; }
+
+    [Networked(OnChanged = nameof(OnNickNameChanged))]
+    public NetworkString<_16> nickName { get; set; }
 
     void Start()
     {
@@ -18,17 +23,41 @@ public class NetWorkPlayer : NetworkBehaviour, IPlayerLeft
         {
             Local = this;
 
-
+            RPC_SetNickName(PlayerPrefs.GetString("PlayerNickname"));
         }
         else
         {
 
         }
+
+        transform.name = $"P_{Object.Id}";
     }
     public void PlayerLeft(PlayerRef player)
     {
         if (player == Object.InputAuthority)
             Runner.Despawn(Object);
+    }
+
+    static void OnNickNameChanged(Changed<NetWorkPlayer> changed)
+    {
+        Debug.Log($"{Time.time} OnHPChanged value {changed.Behaviour.nickName}");
+
+        changed.Behaviour.OnNickNameChanged();
+    }
+
+    private void OnNickNameChanged()
+    {
+        Debug.Log($"Nick Name changed for player to {nickName} for player {gameObject.name}");
+
+        playerNickNameTM.text = nickName.ToString();
+    }
+
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+
+    public void RPC_SetNickName(string nickName, RpcInfo info = default)
+    {
+        Debug.Log($"[RPC] SetNickName {nickName}");
+        this.nickName = nickName;
     }
 
 }
