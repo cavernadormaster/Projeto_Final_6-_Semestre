5cfdfff5c91a87ac9376d7472ff1e4d1d381f354
Felipe Moraes <89394365+cavernadormaster@users.noreply.github.com>
2023-11-29 15:45:57 -0300

    VAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII

--- Assets/Scripts/Player/SetInside.cs
+++ Assets/Scripts/Player/SetInside.cs
@@ -9,7 +9,7 @@ public class SetInside : MonoBehaviour
 
     private void Awake()
     {
-        animator = GetComponent<Animator>();
+        
         CharacterMovementHandler.PreAnim = animator;
         CharacterMovementHandler.anim = true;
     }
