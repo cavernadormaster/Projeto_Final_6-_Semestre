1a920ac03d611c51e00f5ce507c80c2a78ffbfd7
Felipe dos Santos Moraes <89394365+cavernadormaster@users.noreply.github.com>
2023-12-02 21:00:20 -0300

    Morte

--- Assets/Scripts/Player/ZombieInteractions.cs
+++ Assets/Scripts/Player/ZombieInteractions.cs
@@ -4,12 +4,16 @@ using UnityEngine;
 
 public class ZombieInteractions : MonoBehaviour
 {
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Cientista Verde") || other.CompareTag("Cientista Vermelho") || other.CompareTag("Cientista Amarelo") || other.CompareTag("Cientista Azul"))
         {
             InGameManager.matou = true;
             string ip = other.gameObject.name;
+            if (other.GetComponent<CharacterMovementHandler>().HasInputAuthority)
+                GameObject.Find("Morte").SetActive(true);
+
             Debug.Log("Destroy" + ip);
             Destroy(GameObject.Find(ip));
         }
