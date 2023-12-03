f1b3f3c4c402f176d00c76492128865284fef995
Joorgin <88665411+Joorgin@users.noreply.github.com>
2023-12-02 23:50:13 -0300

    FORGUETY ABAUTE DE COMITI

--- Assets/Scripts/Player/ZombieInteractions.cs
+++ Assets/Scripts/Player/ZombieInteractions.cs
@@ -5,25 +5,26 @@ using UnityEngine;
 public class ZombieInteractions : MonoBehaviour
 {
     string ip;
+    CharacterMovementHandler characterMovementHandler;
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Cientista Verde") || other.CompareTag("Cientista Vermelho") || other.CompareTag("Cientista Amarelo") || other.CompareTag("Cientista Azul"))
         {
-            
+
+            characterMovementHandler = other.GetComponent<CharacterMovementHandler>();
+
+            if (!characterMovementHandler.Object.HasInputAuthority)
+                GameObject.Find("Morte").SetActive(true);
+
             ip = other.gameObject.name;
+
             Destroy(GameObject.Find(ip));
-
-         // if(other.GetComponent<CharacterMovementHandler>().Object.HasInputAuthority)
-         //     GameObject.Find("Morte").SetActive(true);
-           //
-           // if(!InGameManager.matou)
-           // StartCoroutine(MorteCountDown());
-
         }
     }
 
     IEnumerator MorteCountDown()
     {
+
         InGameManager.matou = true;
         InGameManager.CientistInGame--;
         
